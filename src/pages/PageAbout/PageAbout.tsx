17ac576ef1bb3d73a4484a301480f02aaf291978
Saylendra Yasin <saylendrayasin@gmail.com>
2024-02-05 18:11:54 +0800

    fix style

--- src/pages/PageAbout/PageAbout.tsx
+++ src/pages/PageAbout/PageAbout.tsx
@@ -42,58 +42,50 @@ export default function PageAbout() {
                   className="alr__img"
                   src={image2}
                   alt="logo"
-                  height={220}
+                  height={216}
                 />
                 <img
                   className="alr__img"
                   src={image3}
                   alt="logo"
-                  height={220}
+                  height={216}
                 />
               </div>
             </div>
             <div className="about__desc">
-              <div className="ad__content">
-                <div className="ad__content-wrapper">
-                  <p className="adc__desc">
-                    I was born in Bitung on October 10, 2002. Originally, I am
-                    from Tobelo, North Halmahera in the eastern part of
-                    Indonesia. I have been living with both of my parents until
-                    now, and they have always supported me in both academic and
-                    non-academic aspects. I attended school in Tobelo until the
-                    age of 15 and then moved to Manado to pursue my high school
-                    education. After graduating from high school in 2020, I
-                    entered college in the same year at Klabat University,
-                    majoring in Informatics. Throughout my academic journey, I
-                    initially found interest in design, particularly UI/UX.
-                    However, my passion evolved, and I developed a keen interest
-                    in game development, specifically in 3D design. Since
-                    childhood, I have always enjoyed playing video games,
-                    leading me to decide to specialize in game development with
-                    the aspiration of becoming a level designer. After
-                    graduating from high school in 2020, I entered college in
-                    the same year at Klabat University, majoring in Informatics.
-                  </p>
-                </div>
-                <div className="ad__content-wrapper">
-                  <p className="adc__desc">
-                    Throughout my academic journey, I initially found interest
-                    in design, particularly UI/UX. However, my passion evolved,
-                    and I developed a keen interest in game development,
-                    specifically in 3D design. Since childhood, I have always
-                    enjoyed playing video games, leading me to decide to
-                    specialize in game development with the aspiration of
-                    becoming a level designer. I like singing and playing the
-                    piano since I was 9. I'm also into music, which is why I
-                    enjoy sounds and music in game development and want to learn
-                    more about it. Looking forward, I am eager to contribute to
-                    the gaming industry, bringing fresh ideas and innovative
-                    designs to life. I envision a future where my skills not
-                    only meet the demands of the evolving game development
-                    landscape but also inspire others to push the boundaries of
-                    creativity.
-                  </p>
-                </div>
+              <div className="ad__content-wrapper">
+                <p className="adc__desc">
+                  I was born in Bitung on October 10, 2002. Originally, I am
+                  from Tobelo, North Halmahera in the eastern part of Indonesia.
+                  I have been living with both of my parents until now, and they
+                  have always supported me in both academic and non-academic
+                  aspects. I attended school in Tobelo until the age of 15 and
+                  then moved to Manado to pursue my high school education. After
+                  graduating from high school in 2020, I entered college in the
+                  same year at Klabat University, majoring in Informatics.
+                  Throughout my academic journey, I initially found interest in
+                  design, particularly UI/UX. However, my passion evolved, and I
+                  developed a keen interest in game development, specifically in
+                  3D design. Since childhood, I have always enjoyed playing
+                  video games, leading me to decide to specialize in game
+                  development with the aspiration of becoming a level designer.
+                  After graduating from high school in 2020, I entered college
+                  in the same year at Klabat University, majoring in
+                  Informatics. Throughout my academic journey, I initially found
+                  interest in design, particularly UI/UX. However, my passion
+                  evolved, and I developed a keen interest in game development,
+                  specifically in 3D design. Since childhood, I have always
+                  enjoyed playing video games, leading me to decide to
+                  specialize in game development with the aspiration of becoming
+                  a level designer. I like singing and playing the piano since I
+                  was 9. I'm also into music, which is why I enjoy sounds and
+                  music in game development and want to learn more about it.
+                  Looking forward, I am eager to contribute to the gaming
+                  industry, bringing fresh ideas and innovative designs to life.
+                  I envision a future where my skills not only meet the demands
+                  of the evolving game development landscape but also inspire
+                  others to push the boundaries of creativity.
+                </p>
               </div>
             </div>
           </div>
